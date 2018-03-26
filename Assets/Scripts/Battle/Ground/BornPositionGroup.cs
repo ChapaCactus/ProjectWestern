afc151955c5eb30f05e580f4c8d47a5f0c394c18
Chapa.C(Takuya) <chapatown@gmail.com>
2018-03-27 00:16:16 +0900

    敵出現位置設定クラス実装

---
+++ Assets/Scripts/Battle/Ground/BornPositionGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+namespace CCG
+{
+	[Serializable]
+	public class BornPositionGroup
+	{
+		[SerializeField]
+		private List<Transform> _bornPosList = new List<Transform>();
+
+		public Vector3 GetRandomPos()
+		{
+			var randomTF = _bornPosList.OrderBy(index => Guid.NewGuid()) as Transform;
+			return randomTF.localPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/Battle/Ground/GroundSetting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+	public class GroundSetting : MonoBehaviour
+	{
+		[SerializeField]
+		private BornPositionGroup _enemyBornPosGroup = new BornPositionGroup();
+	}
+}
