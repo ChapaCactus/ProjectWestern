06c140f059686262a36af765b9b50dab8fcd20ae
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-03 18:57:06 +0900

    実体(インタンスされた要素)の格納Containerクラス作成

---
+++ Assets/Scripts/Utilities/EntityContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CCG
+{
+	public class EntityContainer<T>
+	{
+		public EntityContainer()
+		{
+			Init();
+		}
+
+		public Queue<T> Entities { get; private set; }
+
+		public bool IsEmpty => (Entities.Count == 0);
+
+		public void Init()
+		{
+			Entities = new Queue<T>();
+		}
+
+		public void Set(T arg)
+		{
+			Entities.Enqueue(arg);
+		}
+
+		public void Pick(Action<T> resFunc)
+		{
+			T picked = Entities.Dequeue();
+			resFunc.SafeCall(picked);
+		}
+	}
+}
