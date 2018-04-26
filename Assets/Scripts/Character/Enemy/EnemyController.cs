5f9b12a1f224ea24ab260e3911531387b5d48dde
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-26 19:21:24 +0900

    ソース簡略化、文字コード修正

--- Assets/Scripts/Character/Enemy/EnemyController.cs
+++ Assets/Scripts/Character/Enemy/EnemyController.cs
@@ -6,70 +6,66 @@ using UnityEngine;
 
 namespace CCG
 {
-	[RequireComponent(typeof(EnemyView))]
-	public class EnemyController : CharacterController
-	{
-		private static readonly string PrefabName = "Enemy";
-		private static readonly string PrefabDirPath = "Prefabs/Enemy";
+    [RequireComponent(typeof(EnemyView))]
+    public class EnemyController : CharacterController
+    {
+        private static readonly string PrefabName = "Enemy";
+        private static readonly string PrefabDirPath = "Prefabs/Enemy";
 
-		public string Name => _model.Name;
-		public bool IsSleep { get; private set; } = false;
+        public string Name => _model.Name;
+        public bool IsSleep { get; private set; } = false;
 
-		private EnemyModel _model { get; set; }
-		private EnemyView _view { get; set; }
+        private EnemyModel _model { get; set; }
+        private EnemyView _view { get; set; }
 
-		private PlayerController _target { get; set; }
+        private PlayerController _target { get; set; }
 
-		private void Awake()
-		{
-			_view = GetComponent<EnemyView>();
-		}
+        private void Awake()
+        {
+            _view = GetComponent<EnemyView>();
+        }
 
-		private void LateUpdate()
-		{
-			if(_target != null)
-			{
-				Move(_target.transform.position);
-			}
-		}
+        private void LateUpdate()
+        {
+            Move(_target.transform.position);
+        }
 
-		public static void Create(Transform parent, Action<EnemyController> onCreate)
-		{
-			var prefabPath = $"{PrefabDirPath}/{PrefabName}";
-			var prefab = Resources.Load(prefabPath) as GameObject;
-			var go = Instantiate(prefab, parent);
+        public static void Create(Transform parent, Action<EnemyController> onCreate)
+        {
+            var prefabPath = $"{PrefabDirPath}/{PrefabName}";
+            var prefab = Resources.Load(prefabPath) as GameObject;
+            var enemy = Instantiate(prefab, parent).GetComponent<EnemyController>();
+            onCreate.SafeCall(enemy);
+        }
 
-			var enemy = go.GetComponent<EnemyController>();
+        public void Setup(EnemyMaster master)
+        {
+            _view.SetSprite(master.Sprites[0]);
 
-			onCreate.SafeCall(enemy);
-		}
+            _model = new EnemyModel(master);
+        }
 
-		public void Setup(EnemyMaster master)
-		{
-			_view.SetSprite(master.Sprites[0]);
+        public void SetTarget(PlayerController player)
+        {
+            _target = player;
+        }
 
-			_model = new EnemyModel(master);
-		}
+        private void Move(Vector3 targetPos)
+        {
+            if (_target == null) return;
 
-		public void SetTarget(PlayerController player)
-		{
-			_target = player;
-		}
+            var dir = (targetPos - transform.position).normalized;
+            transform.position += dir * (_model.MoveSpeed / 100);
+        }
 
-		private void Move(Vector3 targetPos)
-		{
-			var dir = (targetPos - transform.position).normalized;
-			transform.position += dir * (_model.MoveSpeed / 100);
-		}
-
-		private void OnTriggerEnter2D(Collider2D collision)
-		{
-			if(collision.CompareTag("Bullet"))
-			{
-				var bullet = collision.GetComponent<BulletController>();
-				var power = bullet.Power;
-				Damage(power);
-			}
-		}
-	}
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Bullet"))
+            {
+                var bullet = collision.GetComponent<BulletController>();
+                var power = bullet.Power;
+                Damage(power);
+            }
+        }
+    }
 }
