ee0370652576713d52fe006c176c6a6456cec463
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-28 20:20:43 +0900

    StageMaster読み込み実装

--- Assets/Scripts/StageSelect/StageSelect.cs
+++ Assets/Scripts/StageSelect/StageSelect.cs
@@ -1,14 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CCG.Master;
+using CCG.Enums;
 
 namespace CCG
 {
     public class StageSelect : MonoBehaviour
     {
+        private static readonly string MasterDirPath = "Master/Stage";
+
         public void OnSelectedStage(StageMaster selected)
         {
+        }
+
+        private void LoadStageMaster(StageID id, Action<StageMaster> onLoad)
+        {
+            var path = $"{MasterDirPath}/{id}";
+            var master = Resources.Load(path) as StageMaster;
+
+            onLoad.SafeCall(master);
         }
     }
 }
