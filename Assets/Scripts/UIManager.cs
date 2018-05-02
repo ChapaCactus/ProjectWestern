1d4f690fe70832473dfcfcc65a15bb5509919118
Chapa.C(Takuya) <chapatown@gmail.com>
2018-05-02 15:08:34 +0900

    UIManagerからステージシーンのCanvasPrefabインスタンスを返すように

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Assertions;
@@ -17,10 +17,21 @@ namespace CCG
 
         public RoundTimer RoundTimer => _roundTimer;
 
+        private static readonly string CanvasPathHeader = "Prefabs/UI";
+        private static readonly string StageCanvasPrefabName = "StageCanvas";
+
         private void Awake()
         {
             Assert.IsNotNull(_totalCoinText);
             Assert.IsNotNull(_roundTimer);
+        }
+
+        public static void CreateStageCanvas(Transform parent, Action<Canvas> onCreate)
+        {
+            var prefab = Resources.Load($"{CanvasPathHeader}/{StageCanvasPrefabName}") as GameObject;
+            var canvas = Instantiate(prefab, parent).GetComponent<Canvas>();
+
+            onCreate.SafeCall(canvas);
         }
 
         public void Init()
